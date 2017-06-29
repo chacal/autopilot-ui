b306f93fc685e6091386116774dc8351072a0dea
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-06-29 09:01:03 +0300

    Add first version of MQTT connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react'
+import mqtt = require('mqtt')
+import Client = mqtt.Client
+
 import './App.css'
 
 class App extends React.Component<{}, {}> {
+  mqttClient: Client
+
+  constructor() {
+    super()
+    this.mqttClient = this.startMqttClient('ws://mqtt-home.chacal.fi:8883')
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <div>Render UI here</div>
     )
+  }
+
+  startMqttClient(brokerUrl: string) {
+    const mqttClient = mqtt.connect(brokerUrl)
+    mqttClient.on('connect', () => {
+      mqttClient.subscribe('/sensor/+/+/state')
+      mqttClient.on('message', this.onMqttMessage.bind(this))
+    })
+    return mqttClient
+  }
+
+  onMqttMessage(topic: string, message: Buffer) {
+    const [, , instance, tag, ] = topic.split('/')
+    console.log('Got message:', instance, tag)
   }
 }
 
